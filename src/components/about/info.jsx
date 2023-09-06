f4fa3e9f2ff4f078dcd931a43b9282c8c1dff0fc
Armazok <nikolai.borsin@mail.ru>
2023-09-06 14:35:04 +0300

    ref

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -17,7 +17,7 @@ export const Info = memo(({}) => {
                 <i className='bx bx-briefcase-alt about__icon'></i>
 
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">+3 Project</span>
+                <span className="about__subtitle">+5 Project</span>
 
             </div>
 
--- src/components/work/data/Data.jsx
+++ src/components/work/data/Data.jsx
@@ -1,26 +1,36 @@
-import Work1 from "../../../assets/todo.png"
-import Work2 from "../../../assets/cards.png"
-import Work3 from "../../../assets/inc.png"
-import Work4 from "../../../assets/social.png"
-import Work5 from "../../../assets/jordan.webp"
-import Work6 from "../../../assets/dry.webp"
-import Work7 from "../../../assets/iman.webp"
+import Work1 from "../../../assets/dataProject/todo.png"
+import Work2 from "../../../assets/dataProject/cards.png"
+import Work3 from "../../../assets/dataProject/cygan.png"
+import Work5 from "../../../assets/dataProject/jordan.webp"
+import Work6 from "../../../assets/dataProject/dry.webp"
+import Work7 from "../../../assets/dataProject/iman.webp"
+import Work8 from "../../../assets/dataProject/doggie.png"
+import Work9 from "../../../assets/dataProject/food.png"
 
 export const projectsData = [
-    {id: 1, image: Work3, title: "Inctagram", category: "web", link: 'https://inctagram-develop.vercel.app/'},
+    {id: 1, image: Work3, title: "Inctagram", category: "web", link: 'https://cygan.lol/ru'},
     {id: 2, image: Work2, title: "Flash Card", category: "web", link: 'https://tkachiko.github.io/flashcards/'},
     {id: 3, image: Work1, title: "Todolists", category: "web", link: 'https://armazok.github.io/TrainingTrello/'},
+
     // {id: 4, image: Work4, title: "Social Network", category: "web", link: 'https://armazok.github.io/SocialNetwork/'},
+
     {id: 5, image: Work5, title: "Jordan Piterson | ChatGPT и рассвет компьютеризированного гипер-интеллекта", category: "KlausVox",
         link: 'https://youtu.be/UmdGraG8afg'},
     {id: 6, image: Work6, title: "Dry Creek Wrangler School | Когда у тебя просто не все на своих местах", category: "KlausVox",
         link: 'https://youtu.be/NmSQWrOItwI'},
     {id: 7, image: Work7, title: "Иман Гаджи | 3 книги, которые сделали меня миллионером в 18 лет", category: "KlausVox",
         link: 'https://youtu.be/mW7xY1x2k9g'},
+
+    {id: 8, image: Work8, title: "Doggie", category: "Marcup",
+        link: 'https://marcup-doggie.vercel.app/'},
+
+    {id: 9, image: Work9, title: "Healthy-Food", category: "Marcup",
+        link: 'https://marcup-healthy-food.vercel.app/'},
 ];
 
 export const projectsNav = [
     {name: "All"},
     {name: "Web"},
+    {name: "Marcup"},
     {name: "KlausVox"},
 ]
